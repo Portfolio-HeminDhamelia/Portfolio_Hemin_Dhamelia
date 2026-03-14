0c5a224bb94178a6ddc210a4ad41ff478068222c
Hemin Dhamelia <dhamelia.h@northeastern.edu>
2026-03-13 23:13:32 -0400

    Adding Images to the Tech Stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -14,13 +14,12 @@ import {
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
   "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/python.png",
+  "/images/terraform.png",
+  "/images/css.png",
+  "/images/docker.png",
+  "/images/kubernetes.png",
+  "/images/github.png"
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
